027a52da8229d6ab875d4596a1bd721c945be517
王利华(实习生) <i-wanglihua@autonavi.com>
2014-01-17 18:26:41 +0800

    add oauth2&packurl&statuses&messages

--- config/weiboUrls.js
+++ config/weiboUrls.js
@@ -1,5 +1,20 @@
 var weiUrls={
-  "authorize":"https://api.weibo.com/oauth2/"
+    "oauth2":{
+        "host":"api.weibo.com",
+        "path":"/oauth2/",
+        "protocol":"https://"
+    },
+    "messages":{
+        "host":"m.api.weibo.com",
+        "path":"/2/messages/",
+        "protocol":"https://"
+    },
+    "statuses":{
+        "host":"api.weibo.com",
+        "path":"/2/statuses/",
+        "protocol":"https://"
+    }
 };
 
 exports.weiUrls=weiUrls;
+
