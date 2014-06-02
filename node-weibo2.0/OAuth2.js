6b4584c29b105d97f760798e503a0156104a0a64
unknown <wlhmyit@126.com>
2014-06-02 16:52:02 +0800

    delete dynamic func

--- node-weibo2.0/OAuth2.js
+++ node-weibo2.0/OAuth2.js
@@ -1,10 +1,11 @@
 
 var child_pro = require('child_process');
 var setting = require('./config/setting.json');
+var urlconfig = require('./config/config.json');
 
-
-var Weibo = (function(){
-	var Weibo = {};	
+var Weibo = {};	
+(function(){
+	var paras = urlconfig;
 	//配置文件key
 	Weibo.appKey = {
 		"appKey":setting.appKey,
@@ -17,8 +18,7 @@ var Weibo = (function(){
 		for(var key in paras){
 			arr.push(key + '=' + paras[key]);
 		}
-		var path = '?client_id=' + Weibo.appKey.appKey + '&redirect_uri='
-		           + Weibo.appKey.redirectUrl;
+		var path = '?client_id=' + Weibo.appKey.appKey + '&redirect_uri=' + Weibo.appKey.redirectUrl;
 		return path + arr.join('&');
 	};
 
@@ -37,27 +37,6 @@ var Weibo = (function(){
 		child_pro.exec('start '+path);
 	};
 
-	var paras = [
-		{
-			"Test":[
-				{
-					"func":"authorize",
-					"url":"https://api.weibo.com/oauth2/authorize",
-					"method":["GET","POST"]
-				},
-				{
-					"func":"access_token",
-					"url":"https://api.weibo.com/oauth2/access_token",
-					"method":["POST"]
-				},
-				{
-					"func":"get_token_info",
-					"url":"https://api.weibo.com/oauth2/get_token_info",
-					"method":["POST"]
-				}
-			]
-		}
-	];
 	for(var classKey in paras){
 		//获取class
 		var classBody = paras[classKey];
@@ -67,19 +46,23 @@ var Weibo = (function(){
 		}
 		Weibo[className] = function(){};
 		for(var funcName in classBody){
-			var funcBody = classBody[funcName]
+			var funcBody = classBody[funcName];
 			for(var f in funcBody){
-				Weibo[className][funcBody[f].func] = function(){
-					return funcBody[f].url;
+				Weibo[className][funcBody[f].func] = function(){				
+					
 				};
 			}
 		}
 	}
-	return Weibo;
 })();
 
 
+console.log(Weibo.Test.authorize.test());
+console.log();
+console.log(Weibo.Test.access_token());
 
 
-Weibo.authorize();
+
+
+
 
